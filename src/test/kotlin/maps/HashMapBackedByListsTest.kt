94c3a6706e229aca4a6560ed8fd8f2b0b01726ee
Konstantinos Gkoutzis <kgk@ic.ac.uk>
2024-02-16 17:37:19 +0000

    Original skeleton

---
+++ src/test/kotlin/maps/HashMapBackedByListsTest.kt
@@ -0,0 +1,6 @@
+package maps
+
+class HashMapBackedByListsTest : CustomMutableMapTest() {
+    override fun <K, V> emptyMap(): CustomMutableMap<K, V> =
+        TODO("Replace with call to HashMapBackedByLists constructor ") // HashMapBackedByLists()
+}
---
+++ src/test/kotlin/maps/ListBasedMapTest.kt
@@ -0,0 +1,6 @@
+package maps
+
+class ListBasedMapTest : CustomMutableMapTest() {
+    override fun <K, V> emptyMap(): CustomMutableMap<K, V> =
+        TODO("Replace with call to ListBasedMap constructor") // ListBasedMap()
+}
