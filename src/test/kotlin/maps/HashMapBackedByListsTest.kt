0800907a4af7329fc48eb36053cd9dbc2a774ed6
Mehmet Nur <mkn123@willow12.doc.ic.ac.uk>
2024-02-23 17:53:26 +0000

    last_draft

--- src/test/kotlin/maps/HashMapBackedByListsTest.kt
+++ src/test/kotlin/maps/HashMapBackedByListsTest.kt
@@ -2,5 +2,5 @@ package maps
 
 class HashMapBackedByListsTest : CustomMutableMapTest() {
     override fun <K, V> emptyMap(): CustomMutableMap<K, V> =
-        TODO("Replace with call to HashMapBackedByLists constructor ") // HashMapBackedByLists()
+        HashMapBackedByLists()
 }
--- src/test/kotlin/maps/ListBasedMapTest.kt
+++ src/test/kotlin/maps/ListBasedMapTest.kt
@@ -2,5 +2,5 @@ package maps
 
 class ListBasedMapTest : CustomMutableMapTest() {
     override fun <K, V> emptyMap(): CustomMutableMap<K, V> =
-        TODO("Replace with call to ListBasedMap constructor") // ListBasedMap()
+        ListBasedMap()
 }
